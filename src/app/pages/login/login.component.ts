bc43a61f6a373b06345bb38cbce45bf35137c1b5
A Venkatesh <avenkateshanand1998@gmail.com>
2024-10-16 09:25:00 +0530

    created a login components and login form and the functionality

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  loginObj = {
+    username: '',
+    password: '',
+  };
 
+  apiUrl = 'https://projectapi.gerasim.in/api/EmployeeManagement/';
+  http = inject(HttpClient);
+  route = inject(Router);
+
+  onLogin() {
+    return this.http
+      .post(this.apiUrl + 'login', this.loginObj)
+      .subscribe((res: any) => {
+        if (res.result) {
+          localStorage.setItem('employee', JSON.stringify(res.data));
+          this.route.navigate(['/Dashboard']);
+        } else {
+          alert(res.message);
+        }
+      });
+  }
 }
