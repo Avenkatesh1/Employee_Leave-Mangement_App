2cd0c7c3b3b36655091c82988389cf06386cc59c
A Venkatesh <avenkateshanand1998@gmail.com>
2024-10-16 08:39:07 +0530

    Created a Components, Services, Models and Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { EmployeeComponent } from './pages/employee/employee.component';
+import { NewLeaveComponent } from './pages/new-leave/new-leave.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        redirectTo:"Login",
+        pathMatch:"full"
+    },
+    {
+        path:"Login",
+        component: LoginComponent,
+    },
+    {
+        path:"",
+        component: LayoutComponent,
+        children:[
+            {
+                path:"Dashboard",
+                component: DashboardComponent,
+            },
+            {
+                path:"Employee",
+                component: EmployeeComponent,
+            },
+            {
+                path:"New-leave",
+                component: NewLeaveComponent,
+            },
+        ]
+    }
+];
